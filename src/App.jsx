880284719ad57bab97e5eaaf048c3b7a05a441eb
Qulamali <z.qulamali@gmail.com>
2024-07-03 00:14:59 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TicTacToe from './components/TicTacToe'
+import './index.scss'
+function App() {
+  return (
+    <>
+      <div>
+        <TicTacToe />
+      </div>
+    </>
+  )
+}
+
+export default App
